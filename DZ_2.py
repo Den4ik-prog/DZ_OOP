e48074c11cec482683c7395750bb70ac37070528
Denis Kl <des1989@list.ru>
2023-12-13 21:47:01 +0300

    add result.txt

--- DZ_2.py
+++ DZ_2.py
@@ -14,13 +14,13 @@ files_lens = [lines_1, lines_2, lines_3]
 files_lens_sort = sorted(files_lens) #сортируем
 
 with open('result.txt', 'a', encoding='utf-8') as file:
-    file.write(f'файл: 2.txt\n\nколичество строк: {str(files_lens_sort[0])}\n\n')
+    file.write(f'файл: 2.txt \n\n количество строк: {str(files_lens_sort[0])}\n\n')
 
 with open('2.txt', 'r', encoding='utf-8') as first_file, open('result.txt', 'a', encoding='utf-8') as second_file: #содержимое файла добавляем
     counting = 0
     for line in first_file:
         counting += 1
-        second_file.write(f'строка №  {counting}  {line}')
+        second_file.write(f'строка № {counting} {line}')
     second_file.write('\n\n')
 
 with open('result.txt', 'a', encoding='utf-8') as file:  
@@ -30,15 +30,15 @@ with open('1.txt', 'r', encoding='utf-8') as first_file, open('result.txt', 'a',
     counting = 0
     for line in first_file:
         counting += 1
-        second_file.write(f'строка №  {counting}  {line}')
+        second_file.write (f'строка № {counting} {line}')
     second_file.write('\n\n')
 
 with open('result.txt', 'a', encoding='utf-8') as file: 
-    file.write(f'файл: 3.txt\n\nколичество строк: {str(files_lens_sort[2])}\n\n')
+    file.write(f'файл: 3.txt\n\n количество строк: {str(files_lens_sort[2])}\n\n')
 
 with open('3.txt', 'r', encoding='utf-8') as first_file, open('result.txt', 'a', encoding='utf-8') as second_file: #содержимое файла добавляем
     counting = 0
     for line in first_file:
         counting += 1
-        second_file.write(f'строка №  {counting}  {line}')
+        second_file.write(f'строка № {counting} {line}')
     second_file.write('\n\n')
